fc52cd86ab05d5af061bee5d520e64e5023d5f58
Forketyfork <forketyfork@icloud.com>
2026-01-28 16:06:20 +0100

    chore: release v0.3.0

--- flake.nix
+++ flake.nix
@@ -89,7 +89,7 @@ EOF
         # Package definition for marx
         packages.default = pkgs.python312Packages.buildPythonApplication {
           pname = "marx";
-    version = "0.2.0";
+    version = "v0.3.0";
 
           src = ./.;
 
